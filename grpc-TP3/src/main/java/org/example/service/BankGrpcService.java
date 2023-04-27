4768271347d41ee29354d72012e196661c370909
Oumaïma <120368654+oumaimabenaboud@users.noreply.github.com>
2023-04-27 08:34:49 +0100

    Initial commit

---
+++ grpc-TP3/src/main/java/org/example/service/BankGrpcService.java
@@ -0,0 +1,37 @@
+package org.example.service;
+
+import io.grpc.stub.StreamObserver;
+import org.example.stubs.Bank;
+import org.example.stubs.BankServiceGrpc;
+
+public class BankGrpcService extends BankServiceGrpc.BankServiceImplBase {
+    @Override
+    public void convert(Bank.ConvertCurrencyRequest request, StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
+        String currencyFrom=request.getCurrencyFrom();
+        String currencyTo=request.getCurrencyTo();
+        double amount=request.getAmount();
+        Bank.ConvertCurrencyResponse response = Bank.ConvertCurrencyResponse.newBuilder()
+                .setCurrencyFrom(currencyFrom)
+                .setCurrencyTo(currencyTo)
+                .setAmount(amount)
+                .setResult(amount*11.4)
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getCurrencyStream(Bank.ConvertCurrencyRequest request, StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
+        super.getCurrencyStream(request, responseObserver);
+    }
+
+    @Override
+    public StreamObserver<Bank.ConvertCurrencyRequest> performStream(StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
+        return super.performStream(responseObserver);
+    }
+
+    @Override
+    public StreamObserver<Bank.ConvertCurrencyRequest> fullCurrencyStream(StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
+        return super.fullCurrencyStream(responseObserver);
+    }
+}
