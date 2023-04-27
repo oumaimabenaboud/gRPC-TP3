c41026d7ffca56aa1cc1b12f7e0a1510e4f1a6f5
Oumaïma <120368654+oumaimabenaboud@users.noreply.github.com>
2023-04-27 23:21:32 +0100

    deuxieme partie

--- gRPC_Chat_Client_Python/grpcChatClientPython.py
+++ gRPC_Chat_Client_Python/grpcChatClientPython.py
@@ -1,30 +1,32 @@
 import grpc
 import chat_pb2
 import chat_pb2_grpc
-from datetime import datetime
 
-# Create a channel to the gRPC server
-channel = grpc.insecure_channel('localhost:5555')
+def chat():
+    # Create a channel to the gRPC server
+    channel = grpc.insecure_channel('localhost:5555')
 
-# Create a stub for the service
-stub = chat_pb2_grpc.ChatStub(channel)
+    # Create a stub for the service
+    stub = chat_pb2_grpc.ChatStub(channel)
 
-# Define a function to handle incoming messages
-def handle_incoming_messages(response_iterator):
-    for response in response_iterator:
-        print(f"[{datetime.fromtimestamp(response.timestamp.seconds)}] {response.user_id}: {response.text}")
+    # Prompt the user to enter a stream of messages
+    print("Enter a stream of messages (type 'exit' to end the stream):")
+    while True:
+        # Read a message from the user
+        message_text = input("> ")
+        if message_text == "exit":
+            break
 
-# Open a bidirectional stream and start sending messages
-stream = stub.Chat()
-while True:
-    message_text = input("Enter a message (or 'exit' to quit): ")
-    if message_text.lower() == 'exit':
-        break
-    message = chat_pb2.Message(user_id='user1', text=message_text)
-    stream.on_next(message)
+        # Create a message object
+        message = chat_pb2.Message(
+            user_id='Python Client',
+            text=message_text
+        )
 
-# Close the stream
-stream.on_completed()
+        # Send the message to the server and print the response
+        response = stub.Chat(iter([message]))
+        for message in response:
+            print(message)
 
-# Wait for incoming messages to finish
-handle_incoming_messages(stream)
+if __name__ == '__main__':
+    chat()
