fc1c3dee823e820341e8a0e013facfcf128904ee
Oumaïma <120368654+oumaimabenaboud@users.noreply.github.com>
2023-04-27 10:01:25 +0100

    init

--- grpc-TP3/src/main/java/org/example/service/BankGrpcService.java
+++ grpc-TP3/src/main/java/org/example/service/BankGrpcService.java
@@ -4,6 +4,9 @@ import io.grpc.stub.StreamObserver;
 import org.example.stubs.Bank;
 import org.example.stubs.BankServiceGrpc;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class BankGrpcService extends BankServiceGrpc.BankServiceImplBase {
     @Override
     public void convert(Bank.ConvertCurrencyRequest request, StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
@@ -22,12 +25,54 @@ public class BankGrpcService extends BankServiceGrpc.BankServiceImplBase {
 
     @Override
     public void getCurrencyStream(Bank.ConvertCurrencyRequest request, StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
-        super.getCurrencyStream(request, responseObserver);
+        String currencyFrom=request.getCurrencyFrom();
+        String currencyTo=request.getCurrencyTo();
+        double amount=request.getAmount();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            private int counter=0;
+            @Override
+            public void run() {
+
+                Bank.ConvertCurrencyResponse response = Bank.ConvertCurrencyResponse.newBuilder()
+                        .setCurrencyFrom(currencyFrom)
+                        .setCurrencyTo(currencyTo)
+                        .setAmount(amount)
+                        .setResult(amount*Math.random()*100)
+                        .build();
+                responseObserver.onNext(response);
+                ++counter;
+                if(counter==20){
+                    responseObserver.onCompleted();
+                    timer.cancel();
+                }
+            }
+        }, 1000, 1000);
     }
 
     @Override
     public StreamObserver<Bank.ConvertCurrencyRequest> performStream(StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
-        return super.performStream(responseObserver);
+        return new StreamObserver<Bank.ConvertCurrencyRequest>() {
+            double sum = 0;
+            @Override
+            public void onNext(Bank.ConvertCurrencyRequest convertCurrencyRequest) {
+                sum+=convertCurrencyRequest.getAmount();
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                Bank.ConvertCurrencyResponse response = Bank.ConvertCurrencyResponse.newBuilder()
+                        .setResult(sum*11.4)
+                        .build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+            }
+        };
     }
 
     @Override
---
+++ grpc_Client/src/main/java/org/example/client/BankGrpcClient3.java
@@ -0,0 +1,34 @@
+package org.example.client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import org.example.stubs.Bank;
+import org.example.stubs.BankServiceGrpc;
+
+import java.io.IOException;
+
+public class BankGrpcClient3 {
+    public static void main(String[] args) throws IOException {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 5555)
+                .usePlaintext()
+                .build();
+        BankServiceGrpc.BankServiceStub asyncstub = BankServiceGrpc.newStub(managedChannel);
+        Bank.ConvertCurrencyRequest request = Bank.ConvertCurrencyRequest.newBuilder()
+                .setCurrencyFrom("DH").setCurrencyTo("USD").setAmount(6500).build();
+        asyncstub.getCurrencyStream(request, new StreamObserver<Bank.ConvertCurrencyResponse>() {
+            @Override
+            public void onNext(Bank.ConvertCurrencyResponse convertCurrencyResponse) {
+                System.out.println("-------------------------------");
+                System.out.println(convertCurrencyResponse.toString());
+                System.out.println("-------------------------------");
+            }
+            @Override
+            public void onError(Throwable throwable) { System.out.println(throwable.getMessage()); }
+            @Override
+            public void onCompleted() { System.out.println("END"); }
+        });
+        System.out.println("......?");
+        System.in.read();
+    }}
+
